df0aac9e62162790b4d916b1dc010bbec0f4caff
amphibian303 <anthonytilotta303@gmail.com>
2021-10-14 19:24:45 -0400

    Tiered Profit Splitter added

--- Starter-Code/TieredProfitSplitter.sol
+++ Starter-Code/TieredProfitSplitter.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.5.0;
 
 // lvl 2: tiered split
 contract TieredProfitSplitter {
-    address payable employee_one; // ceo
-    address payable employee_two; // cto
-    address payable employee_three; // bob
+    address payable employee_one; // CEO
+    address payable employee_two; // CTO
+    address payable employee_three; // Bob
 
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
@@ -22,15 +22,38 @@ contract TieredProfitSplitter {
         uint total;
         uint amount;
 
-        // @TODO: Calculate and transfer the distribution percentage
+        // @TODO: Calculate and transfer the distribution percentage for the CEO
         // Step 1: Set amount to equal `points` * the number of percentage points for this employee
+        amount = points * 60;
+        
         // Step 2: Add the `amount` to `total` to keep a running total
-        // Step 3: Transfer the `amount` to the employee
+        total += amount;
+        
+        // Step 3: Transfer the `amount` to the CEO
+        employee_one.transfer(amount);
+          
 
         // @TODO: Repeat the previous steps for `employee_two` and `employee_three`
         // Your code here!
-
-        employee_one.transfer(msg.value - total); // ceo gets the remaining wei
+        amount = points * 25;
+        
+        total += amount;
+        
+        // transfer the 'amount' to the CTO
+        employee_two.transfer(amount);
+        
+        //////////////////////
+        
+        amount = points * 15;
+        
+        total += amount;
+        
+        // transfer the 'amount' to Bob
+        employee_three.transfer(amount);
+        
+        
+        // Send the remaining wei back to the CEO
+        employee_one.transfer(msg.value - total); // ceo gets the remaining wei back
     }
 
     function() external payable {

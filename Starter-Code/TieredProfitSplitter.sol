890e608a1e35728d5dea5074a92ff24216622a8c
Anthony Tilotta <anthonytilotta303@gmail.com>
2021-12-17 17:57:33 -0500

    Update TieredProfitSplitter.sol

--- Starter-Code/TieredProfitSplitter.sol
+++ Starter-Code/TieredProfitSplitter.sol
@@ -1,29 +1,38 @@
 pragma solidity ^0.5.0;
 
-// lvl 2: tiered split
+// Tiered profit splitting contract
+
 contract TieredProfitSplitter {
+
     address payable employee_one; // CEO
     address payable employee_two; // CTO
     address payable employee_three; // Bob
 
     constructor(address payable _one, address payable _two, address payable _three) public {
+    
         employee_one = _one;
         employee_two = _two;
         employee_three = _three;
     }
 
-    // Should always return 0! Use this to test your `deposit` function's logic
+    // This function should return a value of "0" because all ETH should be sent back to the CEO once profits have been split.
+    
     function balance() public view returns(uint) {
         return address(this).balance;
     }
 
+    // Deposit profits and track the split
+
     function deposit() public payable {
+    
         uint points = msg.value / 100; // Calculates rudimentary percentage by dividing msg.value into 100 units
         uint total;
         uint amount;
 
-        // @TODO: Calculate and transfer the distribution percentage for the CEO
+        // Calculate and transfer the distribution percentage for the CEO
         // Step 1: Set amount to equal `points` * the number of percentage points for this employee
+        
+        // CEO
         amount = points * 60;
         
         // Step 2: Add the `amount` to `total` to keep a running total
@@ -33,17 +42,18 @@ contract TieredProfitSplitter {
         employee_one.transfer(amount);
           
 
-        // @TODO: Repeat the previous steps for `employee_two` and `employee_three`
-        // Your code here!
-        amount = points * 25;
+        // Repeat the previous steps for `employee_two` and `employee_three`
+        
+        // CTO
+        amount = points * 25; 
         
         total += amount;
         
         // transfer the 'amount' to the CTO
         employee_two.transfer(amount);
         
-        //////////////////////
-        
+        // Bob
+
         amount = points * 15;
         
         total += amount;
@@ -51,11 +61,10 @@ contract TieredProfitSplitter {
         // transfer the 'amount' to Bob
         employee_three.transfer(amount);
         
-        
         // Send the remaining wei back to the CEO
-        employee_one.transfer(msg.value - total); // ceo gets the remaining wei back
+        employee_one.transfer(msg.value - total); // CEO gets back any remaining wei
     }
-
+    
     function() external payable {
         deposit();
     }
